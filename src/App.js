c0cd86ca2e8b5dcc7ef4c67f5d69b9069fcd89f1
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-05-04 16:45:07 -0500

    Adding images and provided information to project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import whiskey from "./images/whiskey.jpg"
+import hazel from "./images/hazel.jpg";
+import tubby from "./images/tubby.jpg";
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import NavBar from './NavBar'
 import DogList from './DogList'
 import Dog from './Dog'
 
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  } 
 
+  render() {
 
-function App() {
   return (
     <div className="App">
-        <h1 className="display-4">Large Text</h1>
+        <Route path="/dogs" render={() => <h1 className="display-1"> Dog List</h1>} /> 
     </div>
   );
+  }
 }
 
 export default App;
